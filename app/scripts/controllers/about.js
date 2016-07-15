c0531879cc7df8b7a588dc1d10abf5767e776621
Darin <darinjacosta@gmail.com>
2016-07-14 19:50:06 -0500

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name janeplaceApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the janeplaceApp
+ */
+angular.module('janeplaceApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name janeplaceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the janeplaceApp
+ */
+angular.module('janeplaceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
